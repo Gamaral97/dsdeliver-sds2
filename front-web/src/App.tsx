a9351f335f0fc06df5a6644c2b603be7d238f0f3
Gabriel Amaral <gabriel.amaral9761@gmail.com>
2021-01-10 00:44:24 -0300

    Add Routes and StepsHeader

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,15 +1,11 @@
 import React from 'react';
 import './App.css';
-import Home from './Home';
-import NavBar from './Navbar';
+import Routes from './Routes';
 
 
 function App() {
   return (
-    <div>
-      <NavBar />
-      <Home />
-    </div>
+    <Routes />
   )
 }
 
